734b9687ef646c6b638b6f795a17a32e1671ad60
Pratik <prats.khive@gmail.com>
2015-01-25 11:09:09 -0500

    move constants into interface class

---
+++ src/main/java/com/example/criSampleApp/AppConstants.java
@@ -0,0 +1,13 @@
+package com.example.criSampleApp;
+
+
+public interface AppConstants{
+public static final int TYPE_RED = 0;
+public static final int TYPE_ORANGE = 1;
+public static final int TYPE_YELLOW = 2;
+public static final int TYPE_GREEN = 3;
+public static final int TYPE_BLUE = 4;
+public static final int TYPE_PURPLE = 5;
+public static final int TYPE_WHITE = 6;
+public static final int TYPE_BLACK = 7;
+}
--- src/main/java/com/example/criSampleApp/CardViewAdapter.java
+++ src/main/java/com/example/criSampleApp/CardViewAdapter.java
@@ -20,15 +20,8 @@ import retrofit.client.Response;
 import java.util.List;
 
 
-public class CardViewAdapter extends ArrayAdapter {
-    public static final int TYPE_RED = 0;
-    public static final int TYPE_ORANGE = 1;
-    public static final int TYPE_YELLOW = 2;
-    public static final int TYPE_GREEN = 3;
-    public static final int TYPE_BLUE = 4;
-    public static final int TYPE_PURPLE = 5;
-    public static final int TYPE_WHITE = 6;
-    public static final int TYPE_BLACK = 7;
+public class CardViewAdapter extends ArrayAdapter implements AppConstants{
+
 
     IpTest ipTest;
     HeadersTest headersTest;
